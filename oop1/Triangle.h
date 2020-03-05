01852ab5e7fdf02327982f60abe332f482885466
dalvrock <61521357+dalvrock@users.noreply.github.com>
2020-03-05 22:59:16 +0200

    Add files via upload

---
+++ oop1/Triangle.h
@@ -0,0 +1,26 @@
+#pragma once
+class Triangle
+{
+private:
+	double a, b, c;
+public:
+	Triangle(double a, double b, double c);
+	~Triangle();
+
+	double GetA();
+	double GetB();
+	double GetC();
+
+	void SetA();
+	void SetB();
+	void SetC();
+
+	double Area();
+	double Perimeter();
+	long double Angle1();
+	long double Angle2();
+	long double Angle3();
+
+	void Print();
+};
+
---
+++ oop1/oop1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "Triangle.h"
+
+using namespace std;
+
+int main()
+{
+	Triangle ABC(3, 4, 5);
+	ABC.Print();
+}
+
